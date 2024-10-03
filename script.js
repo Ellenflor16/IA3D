32944edf8120b214c4bf819d81c81e8df5dec42d
ellen <110677074+Ellenflor16@users.noreply.github.com>
2024-10-03 08:38:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,31 +5,47 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [
     {
-        enunciado: "a inteligencia artificial prjudica as pessoas",
-        alternativas:["algumas vezes","nao"]
-    },
-    {
-        enunciado: "voce gosta de usar intelicia artificil",
-        alternativas:["sim","nao"]
-    },
-    {
+        enunciado: "a inteligencia artificial prjudica as pessoas?",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmativa: "alternativa da afirmativa 1"
+            },
+            {
+                texto: "não",
+                afirmativa: "afirmativa da alternativa 2"
+            },
+            {
+        enunciado: "voce gosta de usar intelicia artificil?",
+        alternativas: [{
+            texto:"sim", 
+            afirmativa:"afirmativa da alterntiva 1"
+       },
+       {
+            texto:"não",
+            afirmativa:"afirmativa da alternativa 2"
+        }
+    [
+
+    ]
         enunciado: "usa com frequencia",
-        alternativas:["sim ","nao"]
+        alternativas: ["sim", "nao"]
     }
 ]
 
 let posicao = 0;
 let perguntaAtual;
 
-function mostraPergunta(){
+function mostraPergunta() {
     perguntaAtual = perguntas[poicao];
     caixaPergunta.textContent = perguntaAtual.enunciado;
 
 }
-function mostraAlternativa(){
-    for(const alternativa of perguntaAtual.alternativa){
+function mostraAlternativa() {
+    for (const alternativa of perguntaAtual.alternativa) {
         const botaoAlternativa = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativa.textContent = alternativa;
-        caixaAlternativa.appendChild(botaoAlternativas);
-      }
+       
     }
+}
